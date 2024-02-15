0878bdef194ff7ee9cb5394067b800026450a07f
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2024-02-14 23:17:23 -0300

    feature(Design): Adding first version of creditsView

--- GravityVoyager.swiftpm/CreditsView.swift
+++ GravityVoyager.swiftpm/CreditsView.swift
@@ -5,17 +5,70 @@ import SwiftUI
 struct CreditsView: View {
     @EnvironmentObject private var routeManager: NavigationRouter
     var body: some View {
-        ZStack {
+        ZStack{
             Image("backgroundGradient")
                 .resizable()
                 .ignoresSafeArea()
-            VStack(spacing: 20){
+            VStack(spacing: 10){
+                ScrollView{
+                    VStack(spacing: 20){
+                        Text("Sound Effects")
+                            .foregroundColor(.white)
+                            .font(.system(size: 22, weight: .bold))
+                        CreditsContainer(headerName: "Collect Sound", source: "FreeSound", url: "https://freesound.org/people/MATRIXXX_/sounds/523547/", author: "MATRIXXX_")
+                        CreditsContainer(headerName: "Neutralize Sound", source: "fulano", url: "https://freesound.org/people/deathbyfairydust/sounds/658431/", author: "debyfairydust")
+                        CreditsContainer(headerName: "Fail Sound", source: "fulano", url: "https://freesound.org/people/jugraf/sounds/370209/", author: "jugraf")
+                        Text("Source of information")
+                            .foregroundColor(.white)
+                            .font(.system(size: 22, weight: .bold))
+                        CreditsContainer(headerName: "Moon information", source: "Nasa", url: "https://freesound.org/people/MATRIXXX_/sounds/523547/", author: "junior")
+                        CreditsContainer(headerName: "Mars information", source: "Nasa", url: "https://freesound.org/people/MATRIXXX_/sounds/523547/", author: "junior")
+                        CreditsContainer(headerName: "Components information", source: "Nasa", url: "https://freesound.org/people/MATRIXXX_/sounds/523547/", author: "junior")
+                        CreditsContainer(headerName: "Components information", source: "Nasa", url: "https://freesound.org/people/MATRIXXX_/sounds/523547/", author: "junior")
+                    }
+                }
                 Spacer()
-                SecondaryButton(title: "Ok") {
+                PrimaryButton(title: "Ok") {
                     routeManager.popToRoot()
                 }
             }
             .padding(20)
+        }
+        .navigationTitle("Credits")
+    }
+}
+
+struct CreditsContainer: View {
+    let headerName: String
+    let source: String
+    let url: String
+    let author: String
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 8)
+                .foregroundColor(Color.theme.secondary)
+            VStack(alignment: .leading, spacing: 8){
+                Text(headerName)
+                    .font(.system(size: 22, weight: .semibold))
+                HStack {
+                    Text("Source:")
+                        .font(.system(size: 16, weight: .regular))
+                    Spacer()
+                    Link(source, destination: URL(string: url) ?? URL(string: "https://www.google.com/")!)
+                        .underline()
+                        .font(.system(size: 16, weight: .regular))
+                        .multilineTextAlignment(.trailing)
+                }
+                HStack {
+                    Text("Author:")
+                        .font(.system(size: 16, weight: .regular))
+                    Spacer()
+                    Text(author)
+                        .font(.system(size: 16, weight: .regular))
+                }
+            }
+            .padding(16)
+            .foregroundColor(.white)
         }
     }
 }
